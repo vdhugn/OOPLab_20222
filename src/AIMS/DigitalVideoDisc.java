d383046ae9ca43fa9602e5aef13b75eafdc23003
Your Name <you@example.com>
2023-05-29 14:45:27 +0700

    update my branch

--- src/AIMS/DigitalVideoDisc.java
+++ src/AIMS/DigitalVideoDisc.java
@@ -5,6 +5,8 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;	
+	private int id;
+	private static int nbDigitalVideoDiscs = 0;
 	
 	public String getTitle() {
 		return title;
@@ -36,24 +38,7 @@ public class DigitalVideoDisc {
 	public void setCost(float cost) {
 		this.cost = cost;
 	}
-	
-	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-	}
-	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-	}
-	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
-	}
+
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
 		this.title = title;
@@ -61,5 +46,6 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
-	}
+		this.id = ++ nbDigitalVideoDiscs;
+	}		
 }
